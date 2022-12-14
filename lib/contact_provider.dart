caf528c12d901ee7386b5daed0e30e7c26f996e3
Maruf Ahmed <mk6116055@gmail.com>
2022-12-15 03:48:39 +0600

    updated

--- lib/contact_provider.dart
+++ lib/contact_provider.dart
@@ -1,8 +1,12 @@
+import 'dart:io';
+
 import 'package:contact_app/model/contact_model.dart';
 import 'package:flutter/material.dart';
+import 'package:google_ml_vision/google_ml_vision.dart';
 
 
 class ContactProvider extends ChangeNotifier {
+  List<String?> textLines = [];
   List<ContactModel> contactList = [
     ContactModel(id: 1, name: 'ABC', mobile: '01711000001', email: 'abc@gmail.com', streetAddress: 'Kawranbazar, Dhaka', website: 'www.google.com', image: 'images/person.png'),
     ContactModel(id: 2, name: 'DEF', mobile: '01711000002', email: 'def@gmail.com', streetAddress: 'Dhanmondi, Dhaka', website: 'flutter.dev', favorite: true ,image: 'images/person.png'),
@@ -14,5 +18,17 @@ class ContactProvider extends ChangeNotifier {
     final index = contactList.indexOf(contact);
     contactList[index].favorite = !contactList[index].favorite;
     notifyListeners();
+  }
+
+  Future<void> processCardImage(String imagePath) async {
+    textLines.clear();
+    final GoogleVisionImage visionImage = GoogleVisionImage.fromFile(File(imagePath));
+    final TextRecognizer textRecognizer = GoogleVision.instance.textRecognizer();
+    final VisionText visionText = await textRecognizer.processImage(visionImage);
+    for(var block in visionText.blocks) {
+      for(var line in block.lines) {
+        textLines.add(line.text);
+      }
+    }
   }
 }
